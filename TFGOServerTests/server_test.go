f4fb3361cfdebff8068080c63d569d0426bc602e
Brad <bradleeusa@gmail.com>
2018-02-09 22:02:43 -0600

    added more tests to TestUpdateStatus

--- TFGOServerTests/server_test.go
+++ TFGOServerTests/server_test.go
@@ -219,17 +219,17 @@ func TestHandleLoc(t *testing.T) {
     }
 
     // 2. Testing for player count addition to a control point
-    blueCount1 := cp2.BlueCount
+    expBlueCount := cp2.BlueCount + 1
     (&oliver).handleLoc(loc4)
-    if (cp2.BlueCount != blueCount1 + 1) {
-        t.Errorf("TestHandleLoc(2) failed, expected BlueCount %d, got BlueCount %d", blueCount1 + 1, cp2.BlueCount)
+    if (cp2.BlueCount != expBlueCount) {
+        t.Errorf("TestHandleLoc(2) failed, expected BlueCount %d, got BlueCount %d", expBlueCount, cp2.BlueCount)
     }
 
     // 3. Testing for player count subtraction from a control point
-    blueCount2 := cp2.BlueCount
+    expBlueCount = cp2.BlueCount - 1
     (&oliver).handleLoc(loc5)
-    if (cp2.BlueCount != blueCount2 - 1) {
-        t.Errorf("TestHandleLoc(3) failed, expected BlueCount %d, got BlueCount %d", blueCount2 - 1, cp2.BlueCount)
+    if (cp2.BlueCount != expBlueCount) {
+        t.Errorf("TestHandleLoc(3) failed, expected BlueCount %d, got BlueCount %d", expBlueCount, cp2.BlueCount)
     }
 }
 
@@ -240,21 +240,47 @@ func TestUpdateStatus(t *testing.T) {
      */
 
     // 1. Testing control point contest when 2:1 in favor of Blue Team (Oliver and Anders (blue) vs. Jenny (red) at loc4)
-    captureProg1 := cp2.CaptureProgress
+    expCaptureProg := cp2.CaptureProgress + 1
     (&oliver).handleLoc(loc4)
     cp2.updateStatus()
-    time.Sleep(time.Second)
-    if (cp2.CaptureProgress != captureProg1 + 1) {
-        t.Errorf("TestUpdateStatus(1) failed, expected CaptureProgress %d, got CaptureProgress %d", captureProg1 + 1, cp2.CaptureProgress)
+    if (cp2.CaptureProgress != expCaptureProg) {
+        t.Errorf("TestUpdateStatus(1) failed, expected CaptureProgress %d, got CaptureProgress %d", expCaptureProg, cp2.CaptureProgress)
     }
 
     // 2. Testing control point contest when 1:1 (Anders (blue) vs. Jenny (red) at loc4)
-    captureProg2 := cp2.CaptureProgress
+    expCaptureProg = cp2.CaptureProgress
     (&oliver).handleLoc(loc5)
     cp2.updateStatus()
-    time.Sleep(time.Second)
-    if (cp2.CaptureProgress != captureProg2) {
-        t.Errorf("TestUpdateStatus(1) failed, expected CaptureProgress %d, got CaptureProgress %d", captureProg2, cp2.CaptureProgress)
+    if (cp2.CaptureProgress != expCaptureProg) {
+        t.Errorf("TestUpdateStatus(2) failed, expected CaptureProgress %d, got CaptureProgress %d", expCaptureProg, cp2.CaptureProgress)
+    }
+
+    // 3. Testing team point increase with no capture points
+    cp1.ControllingTeam = nil
+    cp2.ControllingTeam = nil
+    expRedPoints := redTeam.Points
+    expBluePoints := blueTeam.Points
+    cp1.updateStatus()
+    cp2.updateStatus()
+    if (redTeam.Points != expRedPoints) {
+        t.Errorf("TestUpdateStatus(3) failed, expected Red Team Points %d, got Red Team Points %d", expRedPoints, redTeam.Points)
+    }
+    if (blueTeam.Points != expBluePoints) {
+        t.Errorf("TestUpdateStatus(3) failed, expected Blue Team Points %d, got Blue Team Points %d", expBluePoints, blueTeam.Points)
+    }
+
+    // 4. Testing team point increase with 2 capture points
+    cp1.ControllingTeam = blueTeam
+    cp2.ControllingTeam = blueTeam
+    expRedPoints = redTeam.Points
+    expBluePoints = blueTeam.Points + 2
+    cp1.updateStatus()
+    cp2.updateStatus()
+    if (redTeam.Points != expRedPoints) {
+        t.Errorf("TestUpdateStatus(4) failed, expected Red Team Points %d, got Red Team Points %d", expRedPoints, redTeam.Points)
+    }
+    if (blueTeam.Points != expBluePoints) {
+        t.Errorf("TestUpdateStatus(4) failed, expected Blue Team Points %d, got Blue Team Points %d", expBluePoints, blueTeam.Points)
     }
 }
 
