65d4d70c25a95b8d6510dfa883c5a57e431601e6
Sam Schlang <hawken@sam-schlangs-macbook-pro-3.local>
2018-02-09 23:07:27 -0600

    Adding some unit tests and expanding UI

---
+++ TFGO/GameViewController.swift
@@ -0,0 +1,37 @@
+//
+//  GameViewController.swift
+//  TFGO
+//
+//  Created by Sam Schlang on 2/9/18.
+//  Copyright © 2018 University of Chicago. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func fireButton(_ sender: UIButton) {
+        //Get Location and heading
+        //Get Weapon information
+        //Send information to server
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TFGOTests/TFGOTests.swift
+++ TFGOTests/TFGOTests.swift
@@ -21,9 +21,24 @@ class TFGOTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testGame() {
+        // Tests Game object setup for invalid inputs by user
+        let game = Game()
+        XCTAssertFalse(game.isValid())
+        game.setID(to: "abc")
+        XCTAssertFalse(game.setName(to: ""))
+        XCTAssertFalse(game.setName(to: "abcdefghijklmnopqrstuvwxyzabcd"))
+        XCTAssertTrue(game.setName(to: "def"))
+        game.setMode(to: .cp)
+        XCTAssertFalse(game.setTimeLimit(to: -1))
+        XCTAssertTrue(game.setTimeLimit(to: 10))
+        XCTAssertFalse(game.setMaxPoints(to: -9))
+        XCTAssertTrue(game.setMaxPoints(to: 30))
+        XCTAssertFalse(game.setMaxPlayers(to: -192))
+        XCTAssertTrue(game.setMaxPlayers(to: 120))
+        XCTAssertFalse(game.setDescription(to: "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901"))
+        XCTAssertTrue(game.setDescription(to: "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"))
+        XCTAssertTrue(game.isValid())
     }
     
     func testPerformanceExample() {
