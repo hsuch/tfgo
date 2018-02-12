32f8a5d59355a1d2b267f0cd5fe0ca40552417e6
Sam Schlang <hawken@sam-schlangs-macbook-pro-3.local>
2018-02-12 14:05:37 -0600

    Adding the game lobby view controller

---
+++ TFGO/GameLobbyViewController.swift
@@ -0,0 +1,115 @@
+//
+//  GameLobbyViewController.swift
+//  TFGO
+//
+//  Created by Sam Schlang on 2/10/18.
+//  Copyright © 2018 University of Chicago. All rights reserved.
+//
+
+import UIKit
+
+class GameLobbyViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateGames()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    private enum Privacy {
+        case isPublic
+        case isPrivate
+    }
+    
+    private var gamesPrivate: [Game] = []
+    private var gamesPublic: [Game] = []
+    private var activeType = Privacy.isPublic
+    
+    private func updateGames() {
+        //Get games from server
+        //gamesPrivate =
+        //gamesPublic =
+    }
+
+    @IBAction func segmentButtonPress(_ sender: UISegmentedControl) {
+        if activeType == .isPublic {
+            activeType = .isPrivate
+        } else {
+            activeType = .isPublic
+        }
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if activeType == .isPublic {
+            return gamesPublic.count
+        }
+        return gamesPrivate.count
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
