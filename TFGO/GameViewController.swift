2ffb7690e819c8143300afe057a85ea290b2f8a5
Zach Saunders <zsaunders1@cs.uchicago.edu>
2018-02-14 15:50:05 -0600

    No rotation on game map for anyone. And that's final :(

--- TFGO/GameViewController.swift
+++ TFGO/GameViewController.swift
@@ -17,17 +17,29 @@ class GameViewController: UIViewController, CLLocationManagerDelegate {
     
     let manager = CLLocationManager()
     
+    var initialized = false
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
-        var region = game_map.region
         
         // we want the most recent position of our user
         let location = locations [0]
+        
+        var region:MKCoordinateRegion
+        
         let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
         
-        region.center = myLocation
-        
-        game_map.setRegion(region, animated: true)
+        if (initialized == false) {
+            let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+            region = MKCoordinateRegionMake(myLocation, span)
+            game_map.isRotateEnabled = false
+            initialized = true
+        }
+        else {
+            region = game_map.region
+            region.center = myLocation
+        }
+
+        game_map.setRegion(region, animated: false)
         self.game_map.showsUserLocation = true
     }
     
@@ -39,6 +51,7 @@ class GameViewController: UIViewController, CLLocationManagerDelegate {
         manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.requestWhenInUseAuthorization()
         manager.startUpdatingLocation()
+        manager.startUpdatingHeading()
     }
     
     
