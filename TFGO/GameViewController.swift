7cdd5a50d978d00e5c46e75c6b9ecff23bfd15d7
Sam Schlang <hawken@sam-schlangs-macbook-pro-3.local>
2018-02-15 17:35:01 -0600

    Get hit talk shit

--- TFGO/GameViewController.swift
+++ TFGO/GameViewController.swift
@@ -52,6 +52,8 @@ class GameViewController: UIViewController, CLLocationManagerDelegate {
         manager.requestWhenInUseAuthorization()
         manager.startUpdatingLocation()
         manager.startUpdatingHeading()
+        
+        runTimer()
     }
     
     
@@ -67,9 +69,18 @@ class GameViewController: UIViewController, CLLocationManagerDelegate {
         updateTimer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(GameViewController.update)), userInfo: nil, repeats: true)
     }
     
+    private var currentHealth = 100
+    
     @objc func update() {
         if gameState.getConnection().sendData(data: LocUpMsg()).isSuccess {
             print(gameState.getUser().getLocation())
+            if currentHealth != gameState.getUserHealth() {
+                currentHealth = gameState.getUserHealth()
+                let alertController = UIAlertController(title: "Temp", message:
+                    "You were hit", preferredStyle: UIAlertControllerStyle.alert)
+                alertController.addAction(UIAlertAction(title: "Ouch", style: UIAlertActionStyle.default,handler: nil))
+                self.present(alertController, animated: true, completion: nil)
+            }
         }
     }
     
