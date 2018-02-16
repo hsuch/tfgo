55531db602da915836b0c7ca1990bf8643ad1f74
Jason Guo <jasonguo@Jasons-MacBook-Pro-3.local>
2018-02-15 20:45:34 -0600

    Finished part of the unit tests

--- TFGO/Game.swift
+++ TFGO/Game.swift
@@ -352,7 +352,7 @@ public class Game {
     }
     
     func isValid() -> Bool {
-        if validName(name), validNumber(of: maxTime), validNumber(of: maxPoints), validNumber(of: maxPlayers), validDescription(description) {
+        if validNumber(of: maxTime), validNumber(of: maxPoints), validNumber(of: maxPlayers), validDescription(description) {
             return true
         }
         return false
