e21c610955a10bd3deb30c01b81bae7a8569aab9
Anders Segerberg <asegerberg@live.com>
2018-02-20 17:42:13 -0600

    added pickup structs, pickup methods, sample pickup test

---
+++ TFGOServer/pickup.go
@@ -0,0 +1,13 @@
+package main
+
+func (p ArmorPickup) use(game *Game, player *Player, location *Location) {
+	player.Armor += p.AP
+}
+
+func (p HealthPickup) use(game *Game, player *Player, location *Location) {
+	player.Health += p.HP
+}
+
+func (p WeaponPickup) use(game *Game, player *Player, location *Location) {
+	player.Weapons[p.WP.Name] = p.WP
+}
