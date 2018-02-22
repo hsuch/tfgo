e04ad9b746a416b039f41eb22b4092b655b2f2ca
Brad <bradleeusa@gmail.com>
2018-02-21 18:02:27 -0600

    fixed testPickupArmor()

--- TFGOServer/pickup_test.go
+++ TFGOServer/pickup_test.go
@@ -1,17 +1,22 @@
 package main
 
-//import "testing"
-//
-//func TestPickupArmor(t *testing.T) {
-//	isTesting = true
-//	g := makeSampleGame()
-//	p := makeArmorPickup(Location{1, 1});
-//	brad := getBrad(g) // (HP 80, AP 30)
-//	bradAP := brad.Armor
-//	p.use(g, brad)
-//	if brad.Armor != bradAP + p.AP {
-//		t.Errorf("%s(%s) failed, expected (Armor: %d), got (Armor: %d)",
-//			"TestPickupArmor", pname, jAP + p.AP, jenny.Armor)
-//	}
-//}
-//
+import "testing"
+
+func TestPickupArmor(t *testing.T) {
+	isTesting = true
+	g := makeSampleGame()
+	p := makeArmorPickup(Location{1, 1});
+	brad := getBrad(g) // (HP 80, AP 30)
+	bradAP := brad.Armor
+	p.use(g, brad)
+	if brad.Armor != bradAP + p.(*ArmorPickup).AP {
+		t.Errorf("TestPickupArmor(1) failed, expected (Armor: %d), got (Armor: %d)",
+			bradAP + p.(*ArmorPickup).AP, brad.Armor)
+	}
+	p.use(g, brad)
+	if brad.Armor != 100 {
+		t.Errorf("%TestPickupArmor(2) failed, expected (Armor: %d), got (Armor: %d)",
+			100, brad.Armor)
+	}
+}
+
