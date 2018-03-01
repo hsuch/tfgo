61ae04a802bcbd5d7e12ef0cb7a90b6bd145ecb1
Sam Schlang <sschlang@nidoran.cs.uchicago.edu>
2018-02-28 18:11:51 -0600

    Adding reload bar and stopping it from crashing

--- TFGO/Game.swift
+++ TFGO/Game.swift
@@ -551,8 +551,12 @@ public class Game {
         return description.count < 100
     }
     
+    private func validBoundaries() -> Bool {
+        return boundaries.count == 4
+    }
+    
     func isValid() -> Bool {
-        if validName(name), validNumber(of: maxTime), validNumber(of: maxPoints), validNumber(of: maxPlayers), validDescription(description) {
+        if validName(name), validNumber(of: maxTime), validNumber(of: maxPoints), validNumber(of: maxPlayers), validDescription(description), validBoundaries() {
             return true
         }
         return false
